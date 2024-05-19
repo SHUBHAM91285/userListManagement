394bdc44cb05a1ef20bbae22f01d0b3c0dad5750
SHUBHAM91285 <tshubhamnath@gmail.com>
2024-05-19 18:03:42 +0530

    get request added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -92,4 +92,37 @@ router.post('/add', jwtAuthMiddleware, async (req, res) => {
     }
 });
 
+router.get('/admin',async(req,res)=>{
+    try{
+        const admin = await Admin.find()
+        const adminRecord = admin.map((data)=>{
+            return{
+                name: data.name,
+                email: data.email
+            }
+        })
+        return res.status(200).json(adminRecord)
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:"Internal Server error"});
+    }
+})
+
+router.get('/users',async(req,res)=>{
+    try{
+        const users = await User.find()
+        const usersRecord = users.map((data)=>{
+            return{
+                name: data.name,
+                email: data.email,
+                city: data.city
+            }
+        })
+        return res.status(200).json(usersRecord)
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:"Internal Server error"});
+    }
+})
+
 module.exports = router;
